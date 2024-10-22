0fd7d4499cac168c57a787b2e8f8b447d8efb90c
Levente Berky <levente.berky@gmail.com>
2024-10-22 16:26:46 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs=[
+            pkgs.nodejs_20
+            pkgs.nodePackages.npm
+          ];
+        };
+      }
+    );
+}
